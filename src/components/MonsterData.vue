488c643fc13655ed004c0457e9c191441f079492
Escias <ljimmy98@hotmail.fr>
2020-09-24 09:59:03 +0200

    filter checkbox

--- src/components/MonsterData.vue
+++ src/components/MonsterData.vue
@@ -2,8 +2,23 @@
     <div>
         <select @change="select" v-model="valueMonster">
             <option v-for="(value, index) in monsters" :value="index" :key="index"> {{ value.name }} </option>
-        </select>
-        <p>{{ selectMonster }} </p>
+        </select><br/>
+        <div class="case">
+            <div class="check">
+                <div class="checkbox"><p>Type</p><input type="checkbox" name="type"/></div>
+                <div class="checkbox"><p>Species</p><input type="checkbox" name="species"/></div>
+                <div class="checkbox"><p>Elements</p><input type="checkbox" name="elements"/></div>
+            </div>
+            <div class="check">
+                <div class="checkbox"><p>Ailments</p><input type="checkbox" name="ailments"/></div>
+                <div class="checkbox"><p>Locations</p><input type="checkbox" name="locations"/></div>
+                <div class="checkbox"><p>Resistances</p><input type="checkbox" name="resistances"/></div>
+            </div>
+            <div class="check">
+                <div class="checkbox"><p>Weaknesses</p><input type="checkbox" name="weaknesses"/></div>
+                <div class="checkbox"><p>Rewards</p><input type="checkbox" name="rewards"/></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -34,5 +49,27 @@
 </script>
 
 <style scoped>
+    .case{
+        border: black solid;
+        margin-left: 20%;
+        margin-right: 20%;
+        margin-top: 1%;
+        background-color: lightgrey;
+    }
 
+    .check{
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    .checkbox{
+        display: flex;
+        align-items: center;
+    }
+
+    span{
+        margin-left: 5%;
+        margin-right: 5%;
+    }
 </style>
