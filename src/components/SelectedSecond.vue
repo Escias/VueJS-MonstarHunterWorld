6c48f0abc80d499b427d7f4f710e6dd10d3a86e2
Rosstail <rosstail_zegeek@hotmail.fr>
2020-09-25 13:55:26 +0200

    added some desc on comparator

--- src/components/SelectedSecond.vue
+++ src/components/SelectedSecond.vue
@@ -1,23 +1,43 @@
 <template>
     <div class="content second" v-if="valueSecondMonster !== -1">
-           <p>
-             Name : {{ selectedSecondMonster.name }}<br/>
-             Species : {{ selectedSecondMonster.species }}<br/>
-             Type : {{ selectedSecondMonster.type }}<br/>
-             Description :{{ selectedSecondMonster.description }}<br/>
-             <br/>
-             Elements : {{ selectedSecondMonster.elements }}<br/>
-             <br/>
-             Ailments: {{ selectedSecondMonster.ailments }}<br/>
-             <br/>
-             Locations: {{ selectedSecondMonster.locations }}<br/>
-             <br/>
-             Resistances: {{ selectedSecondMonster.resistances }}<br/>
-             <br/>
-             Weaknesses: {{ selectedSecondMonster.weaknesses }}<br/>
-             <br/>
-             Rewards: {{ selectedSecondMonster.rewards }}<br/>
-            </p>
+      <p>
+        Name : {{ selectedSecondMonster.name }}<br/>
+        Species : {{ selectedSecondMonster.species }}<br/>
+        Type : {{ selectedSecondMonster.type }}<br/>
+        Description :{{ selectedSecondMonster.description }}
+      </p>
+
+      <p>
+        Elements:<br/>
+      </p>
+      <div v-for="{ item, index } in selectedSecondMonster.elements" :key="index" :item="item" :index="index">
+          {{ selectedSecondMonster.elements }}
+      </div>
+
+      <p>
+        Ailments:<br/>
+      </p>
+      <div v-for="{id, name, description} in selectedSecondMonster.ailments" :key="id" :name="name" :description="description">
+        {{ name }}<br/>
+        {{ description }}
+      </div>
+
+      <p>
+        Locations:<br/>
+      </p>
+      <div v-for="{id, name, zoneCount} in selectedSecondMonster.locations" :key="id" :name="name" :zoneCount="zoneCount">
+        <p>
+          {{ name }}
+        </p>
+      </div>
+      <p>
+        <br/>
+        Resistances: {{ selectedSecondMonster.resistances }}<br/>
+        <br/>
+        Weaknesses: {{ selectedSecondMonster.weaknesses }}<br/>
+        <br/>
+        Rewards: {{ selectedSecondMonster.rewards }}<br/>
+      </p>
     </div>
     <div class="content second" v-else>
       {{ valueSecondMonster }} <br/>
@@ -27,19 +47,19 @@
 
 <script>
 export default {
-        props: {
-          valueSecondMonster: Number,
+    props: {
+      valueSecondMonster: Number,
+    },
+    computed: {
+        selectedSecondMonster(){
+          return this.$store.state.selectedSecondMonster
         },
-        computed: {
-            selectedSecondMonster(){
-              return this.$store.state.selectedSecondMonster
-            },
-            monsters() {
-              return this.$store.state.monsters;
-            },
-        }
-        
+        monsters() {
+          return this.$store.state.monsters;
+        },
     }
+
+}
      
 
 </script>
