6cde66cf839a5fc5fd25698fb80967c645ca5bdc
Theovady Moutty <theomoutty@gmail.com>
2020-09-25 11:40:06 +0200

    add favoris

--- src/components/SelectedFirst.vue
+++ src/components/SelectedFirst.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="content first">
+        
                 <p>
                     {{ selectedFirstMonster.name }}<br/>
                     {{ selectedFirstMonster.species }}<br/>
                     {{ selectedFirstMonster.type }}<br/>
                     {{ selectedFirstMonster.description }}<br/>
+                    {{ selectedFirstMonster.element }}<br/>
                 </p>
     </div>
 </template>
 
 <script>
+import ElementData from '@/components/ElementData.vue'
 export default {
+    components:{
+        ElementData: ElementData,
+    },
     data(){
             return {
                 valueFirstMonster: Number,
